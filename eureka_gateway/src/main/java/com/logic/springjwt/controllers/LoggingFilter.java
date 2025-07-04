3eb7f146b210d8891032b9700eb321600e85e6b0
Nipun Fernando <fernando.wwjpnv@MOF.local>
2025-07-04 09:39:40 +0530

    Jar file names updated

--- eureka_gateway/src/main/java/com/logic/springjwt/controllers/LoggingFilter.java
+++ eureka_gateway/src/main/java/com/logic/springjwt/controllers/LoggingFilter.java
@@ -2,16 +2,35 @@ package com.logic.springjwt.controllers;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import java.net.URI;
 
 @Component
-public class LoggingFilter implements GlobalFilter {
+public class LoggingFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("Incoming request: " + exchange.getRequest().getURI());
-        return chain.filter(exchange);
+        System.out.println("Incoming request URI: " + exchange.getRequest().getURI());
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            URI routedUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+            if (routedUri != null) {
+                System.out.println("Routed to URI: " + routedUri);
+            } else {
+                System.out.println("Routed URI not found in exchange attributes.");
+            }
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println(" ");
+        }));
+
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // Run early in the filter chain
     }
 }
