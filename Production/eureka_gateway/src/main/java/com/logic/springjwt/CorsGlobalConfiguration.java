9b4fb0dee602f2f3b3abd5bf9b26f20646c7a105
Nipun Fernando <fernando.wwjpnv@MOF.local>
2025-08-15 23:03:34 +0530

    Production Mode

--- Production/eureka_gateway/src/main/java/com/logic/springjwt/CorsGlobalConfiguration.java
+++ Production/eureka_gateway/src/main/java/com/logic/springjwt/CorsGlobalConfiguration.java
@@ -16,6 +16,8 @@ public class CorsGlobalConfiguration {
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
+        config.addAllowedOriginPattern("https://itmd.treasury.gov.lk"); // Add multiple allowed origin patterns
+        config.addAllowedOriginPattern("https://systems.treasury.gov.lk"); // Add multiple allowed origin patterns
         config.addAllowedOriginPattern("http://itmd.treasury.gov.lk"); // Add multiple allowed origin patterns
         config.addAllowedOriginPattern("http://systems.treasury.gov.lk"); // Add multiple allowed origin patterns
         config.addAllowedOriginPattern("http://localhost"); // Add multiple allowed origin patterns
